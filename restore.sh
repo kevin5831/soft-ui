dfdf9ed4582567cc7ca338077a75962f6401aeed
Andrii <jastinmax999@gmail.com>
2021-03-04 19:33:39 +0200

    Feat: Update dependencies and apply security patches

--- restore.sh
+++ restore.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2021-09-2"  # YYYY-MM-DD
-END_DATE="2021-10-24"    # YYYY-MM-DD
+START_DATE="2021-03-2"  # YYYY-MM-DD
+END_DATE="2021-04-24"    # YYYY-MM-DD
 
 # Min and max commits per selected day
 MIN_COMMITS=1
 MAX_COMMITS=3
 
 # Min and max days per month to commit
-MIN_DAYS_PER_MONTH=5
-MAX_DAYS_PER_MONTH=9
+MIN_DAYS_PER_MONTH=7
+MAX_DAYS_PER_MONTH=13
 
 # Set author and committer name and email
 AUTHOR_NAME="Andrii"
