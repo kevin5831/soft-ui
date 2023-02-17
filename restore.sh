b5f6e6193fdbe4c28d0b7acc3547dea5756d2e05
Andrii <jastinmax999@gmail.com>
2023-02-17 02:11:38 +0200

    Fix: Improve UI components for better UX and accessibility

--- restore.sh
+++ restore.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2023-07-2"  # YYYY-MM-DD
-END_DATE="2023-07-24"    # YYYY-MM-DD
+START_DATE="2023-02-2"  # YYYY-MM-DD
+END_DATE="2023-02-24"    # YYYY-MM-DD
 
 # Min and max commits per selected day
-MIN_COMMITS=1
-MAX_COMMITS=8
+MIN_COMMITS=15
+MAX_COMMITS=21
 
 # Min and max days per month to commit
-MIN_DAYS_PER_MONTH=7
-MAX_DAYS_PER_MONTH=13
+MIN_DAYS_PER_MONTH=3
+MAX_DAYS_PER_MONTH=9
 
 # Set author and committer name and email
 AUTHOR_NAME="Andrii"
